948a9266c952e4f060b6a072612939966711293b
bdrez <159343619+bdrez@users.noreply.github.com>
2026-01-12 22:43:47 -0500

    Update hashPass.py

--- hashPass.py
+++ hashPass.py
@@ -15,18 +15,21 @@ def user_status():
         #since were converting the response to .lower will be good no matter if they put upper or lower so only check once
         input_new_user()
     else :
-        #can do else bec at the top we made sure only two possibel answers here 
+        #can do else bec at the top we made sure only two possible answers here 
         #call checking function
-        print('input checking function')        
+        print('input checking function')    
+        
 
 def input_new_user():
     user_email =input("enter your email ") #do the string regex to make sure it ends in @gmail @email @yahoo etc .com
     user_pass=input("enter your password ")
     double_pass=input("please enter your password again to verify ")
-    hashCheck(user_pass, double_pass)
+    hashCheck(email, user_pass, double_pass)
 
 
-def hashCheck(a, b):
+def hashCheck(em, a, b):
+    db=defaultdict(str)
+    #create out dict that we will use to store email/password
     md5_pass1=hashlib.md5()
     md5_pass2=hashlib.md5()
     md5_pass1.update(a.encode()) #we need to encode to convert from string to byte so we can hash
@@ -34,9 +37,14 @@ def hashCheck(a, b):
     digest1=md5_pass1.hexdigest()
     digest2=md5_pass2.hexdigest()
     if digest1==digest2:
-        print("passwords match")
+        #if the user enter the password correct both times we store in dict if its a new user/email wasnt used before 
+        if em not in db:
+            db[em]=md5_pass1
     else:
-        print("passwords dont match")
+        print("passwords dont match") 
+        input_new_user() #go back to allow them to enter new information?
     #loop back to reenter password?
 
 user_status()
+
+
