3c3c16a990eb301ba8daf13494f10a73f21ea807
bdrez <159343619+bdrez@users.noreply.github.com>
2026-01-12 20:58:20 -0500

    Update hashPass.py

--- hashPass.py
+++ hashPass.py
@@ -1,9 +1,7 @@
 import hashlib
-'''f
-use a struct or a dict with key values maybe take in the username and pasword then hash the password to store for
+'''
+ take in the username and pasword then hash the password to store for
 first time user and then ask the user to enter the username and password and double check with the hash
-so enter a user name and store enter a password hash and store hash ask to double check password and hash and double 
-check then once in system user can enter infroamtion check if the hash they enter matches and log in
 '''
 
 user_email =input("enter your email ")
@@ -19,4 +17,5 @@ if digest1==digest2:
     print("passwords match")
 else:
     print("passwords dont match")
+
     #loop back to reenter password?
