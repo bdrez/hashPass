775e7e018f36598462f7c2ae045ebd38fe12e374
bdrez <159343619+bdrez@users.noreply.github.com>
2026-01-14 20:42:10 -0500

    Add files via upload

---
+++ saltHash.py
@@ -0,0 +1,4 @@
+#insert salting code here
+''' we ask them to verify password after we will generate a random salt and add that to the dicitonary if the passwords match, 
+then later when they log in we will ask for password and email and combind that password with the salt hash that and check to see 
+if its a match'''
