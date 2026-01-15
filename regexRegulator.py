86a79226b786668ddac95bd6c6ab50e54f05fda1
bdrez <159343619+bdrez@users.noreply.github.com>
2026-01-14 21:28:01 -0500

    Add files via upload

--- regexRegulator.py
+++ regexRegulator.py
@@ -1,1 +1,13 @@
+import re
 #regex regulations to ensure the user only enters a proper email address domain 
+
+def email_ending(ema):
+    #the patter will be the email address domain and the em will be the email were searching 
+    ending=re.search(pattern, ema)
+    #result = find_first('Harker$')
+    if ending!=None:
+        return #valid email we return
+    if ending==None: #means the search was unsucsesful is value is None
+        print("Invalid email address")
+        #go back to enter an email? to log in a user 
+        #gmail.com, email.com, yahoo.com hotmail.com, aol.com  use top five popular ones
