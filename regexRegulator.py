34572cdffdaa5f322863bc4fc6423eae49813b47
bdrez <159343619+bdrez@users.noreply.github.com>
2026-01-14 21:55:07 -0500

    Add files via upload

--- regexRegulator.py
+++ regexRegulator.py
@@ -3,6 +3,9 @@ import re
 
 def email_ending(ema):
     #the patter will be the email address domain and the em will be the email were searching 
+    #punct = "[^a-zA-Z0-9]"
+    pattern="(a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+  @ email.com$|gmail.com$|yahoo.com$|aol.com$|hotmail.com$|icloud.com$)"
+    #pattern=('@email.com|@gmail.com|yahoo.com|aol.com|hotmail.com|icloud.com$')
     ending=re.search(pattern, ema)
     #result = find_first('Harker$')
     if ending!=None:
