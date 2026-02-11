6953640ed8da250b9c2b23376cd9b9cc4213c702
bdrez <159343619+bdrez@users.noreply.github.com>
2026-02-10 22:47:14 -0500

    Add files via upload

--- hashPass.py
+++ hashPass.py
@@ -1,12 +1,10 @@
 import hashlib
 from regexRegulator import email_ending
 import getpass
-''' take in the username and pasword then hash the password to store for
+'''
+ take in the username and pasword then hash the password to store for
 first time user and then ask the user to enter the username and password and double check with the hash
 '''
-
-#maybe make one function to check them
-#change the hash from md5 to a more secure verison 
 
 #create out dict that we will use to store email/password
 db={'b@gmail.com': 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3' }#test case
@@ -14,9 +12,24 @@ db={'b@gmail.com': 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a2
 
 
 def user_status():
+    #give a menu option 
+    #show welcoming message and menu 
+    print("Welcome! Enter the number to chose an option: \n")
+    resp=int(input("1) Create account \n2) Login  \n3) Exit\n"))
+
+    if resp==1:
+        input_new_user()
+    elif resp==2:
+        verify_user()
+    elif resp==3:
+        print("Bye, bye!")
+        return()
+    else:
+        print("Invalid response")
+
     #here we will see if its a first time user and were storing the data or if its an old user logging in 
     #maybe do something abt if they enter nonsense that doesnt start with y or n
-    resp=input("Are you a new user? Enter yes or no: ")
+    #resp=input("Are you a new user? Enter yes or no: ")
     while resp[0].lower()!='y' and resp[0].lower()!='n':
             resp=input("Invalid response. Enter yes for new user or no to log into an existing account: ")
             #resp=input("Are you a new user? ")
@@ -88,7 +101,6 @@ def register_User(em, a):
 def verify_user():
     user_email =input("enter your email ")
     login_atempt=5
-    #flag=False #will use this to flag 
     em_flag=False
     em_flag=email_ending(user_email)#REGEX IMPORTED FUNCTION FROM REGEXREGULATOR
     while em_flag==False:
@@ -127,4 +139,3 @@ def verify_user():
     return
 
 user_status()
-
