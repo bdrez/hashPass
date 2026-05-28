70e689de4637f8bee58e86cd52864c55318f00e9
bdrez <159343619+bdrez@users.noreply.github.com>
2026-05-27 21:59:16 -0400

    Update passwordRules.py

--- passwordRules.py
+++ passwordRules.py
@@ -3,5 +3,5 @@ import re
 #Password must contain 8 Charchters 1 Uppercase letter 1 Lower case letter 1 number 1 Special charchter
 
 def pssWrd(pas):
-    print("Check password against rules")
+    #print("Check password against rules")
     return False #temp
