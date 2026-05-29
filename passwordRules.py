c7b72c03c2373f00c586b5c43f64d6b1a318074e
bdrez <159343619+bdrez@users.noreply.github.com>
2026-05-29 18:03:01 -0400

    Add files via upload

--- passwordRules.py
+++ passwordRules.py
@@ -6,43 +6,45 @@ def pssWrd(pas):
     #print("Check password against rules")
     #or we can just make a general patten and check 
     #firs we have to check length 
-    if len(pas)>=8:
+    while len(pas)>8:
+        print("Password must be 8 charchters minimum.")
+        return False
         #Once its 8 or more charchters we can no check for pattern
         #check pattern seperately so we can clearly say what they are missing 
-        patternUpper="[A-Z]+" #needs to include at least one upper case
-        patternLower="[a-z]+" #needs to include at least one lower case
-        patternNum="[0-9]+" #needs to include at least one number
-        patternSpec="[`~!@#$%^&*()_-+={\'}\]:;\[?/>.<,\"]+" #needs to include one speical charchterhow to include [] "'think need ""
+    patternUpper="[A-Z]+" #needs to include at least one upper case
+    patternLower="[a-z]+" #needs to include at least one lower case
+    patternNum="[0-9]+" #needs to include at least one number
+    patternSpec="[`~!@#$%^&*()_\-\+={\'}\]:;\[?/>.<,\"]+" #needs to include one speical charchterhow to include [] "'think need ""
     #or we can do variables 
     #variables for each to check
-    
 
     #SEE UP RE SEARCHING
-        patUp=re.search(patternUpper, pas)
-        patLo=re.search(patternLower,pas)
-        patNum=re.search(patternNum, pas)
-        patSpe=re.search(patternSpec, pas)
+    patUp=re.search(patternUpper, pas)
+    patLo=re.search(patternLower,pas)
+    patNum=re.search(patternNum, pas)
+    patSpe=re.search(patternSpec, pas)
     #we want to let them know all the check they didnt pass to make a stronger password 
     #so will set a flag called retCount so it wont return on first instance of missing , if the flag is greater than 0 returns false
-        retCount=0
-        if patUp!=None and patLo!=None and patNum!=None and patSpe!=None:
+    retCount=0
+    if patUp!=None and patLo!=None and patNum!=None and patSpe!=None:
             #we get none if the pattern wasnt found, all pattenrs are found the password is good
             #nested inside the length check 
-            return True
-        if patternUpper==None:
-            print("Password needs an uppercase charachter.")
-            retCount+=1
-        if patternLower==None:
-            print("Password needs a lowercase charchter.")
-            retCount+=1
-        if patternNum==None:
-            print("Password needs a number charchter.")
-            retCount+=1
-        if patternSpec==None:
-            print("Password needs a speical charchter. ")
-            retCount+=1
-        if retCount>0:
-            return False
-    print("Password must be 8 charchters minimum.")
+        return True
+    if patUp==None:
+        print("Password needs an uppercase charachter.")
+        retCount+=1
+    if patLo==None:
+        print("Password needs a lowercase charchter.")
+        retCount+=1
+    if patNum==None:
+        print("Password needs a number charchter.")
+        retCount+=1
+    if patSpe==None:
+        print("Password needs a speical charchter. ")
+        retCount+=1
+    if retCount>0:
+        return False
+    else:
+        return True
         
   
